a55b98b2dbd33f86e370e93a40814cb6bd7aff65
EduardsMegnis <A231529EM@rvt.lv>
2024-04-11 13:48:19 +0300

    commit 1.

---
+++ ascii_art_and_animation/MegnisEduards_11_izvade.py
@@ -0,0 +1,14 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> git add OsipovsSanders_10_izvade.PNG
+... print(a, end='\n\n\n\n\n\n\n')
+SyntaxError: invalid syntax
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> print('*******')
+*******
+>>> print('*******')
+*******
---
+++ ascii_art_and_animation/izvade.py
@@ -0,0 +1,3 @@
+print ("#####", end='\n')
+print ("$$$$$", end='\n')
+print ("&&&&&", end='\n')
